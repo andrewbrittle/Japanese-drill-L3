b21ec4d2fe6dffc106b0df8ce03e1a15f1f363c2
andrewbrittle <andrew.brittle@gmail.com>
2026-09-13 10:40:58 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -12,7 +12,7 @@
 // stamp (in index.html) is bumped, so old cached files get cleared out and
 // replaced next time an offline-enabled user is back online. Forgetting
 // this is exactly how a PWA ends up stuck showing someone a stale version.
-const CACHE_VERSION = 'jp-drill-v260912-005';
+const CACHE_VERSION = 'jp-drill-v260912-006';
 
 const PRECACHE_URLS = [
   './',
